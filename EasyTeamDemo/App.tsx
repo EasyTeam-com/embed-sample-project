b9d40f63534759e3aba80e56b7c17375802d770d
Guy Tepper <hey@guytepper.com>
2024-05-08 19:54:41 +0300

    added timesheet screen to navigation stack

--- EasyTeamDemo/App.tsx
+++ EasyTeamDemo/App.tsx
@@ -3,11 +3,12 @@ import { DarkTheme } from '@react-navigation/native';
 import { AUTH_TOKEN } from '@env';
 import { EasyTeamProvider } from '@easyteam/ui';
 
+//#region Navigation related setup
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { HomeScreen } from './screens/HomeScreen';
 import { ClockScreen } from './screens/ClockScreen';
-// import { TimesheetScreen } from './screens/TimesheetScreen';
+import { TimesheetScreen } from './screens/TImesheetScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,15 +19,18 @@ export type RootStackParamList = {
 };
 
 export type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
+//#endregion
+
+const employeesData = []; // fill this with your employees data
 
 function App(): React.JSX.Element {
   return (
-    <EasyTeamProvider token={AUTH_TOKEN}>
+    <EasyTeamProvider token={AUTH_TOKEN} employees={employeesData}>
       <NavigationContainer theme={DarkTheme}>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'EasyTeam UI' }} />
           <Stack.Screen name="Clock" component={ClockScreen} />
-          {/* <Stack.Screen name="Timesheet" component={TimesheetScreen} /> */}
+          <Stack.Screen name="Timesheet" component={TimesheetScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </EasyTeamProvider>
