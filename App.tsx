834237d6eba9ee6b1b978d50413cc64c399e130f
Guy Tepper <hey@guytepper.com>
2024-05-09 10:01:19 +0300

    Moved demo to root folder

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { DarkTheme } from '@react-navigation/native';
-import { AUTH_TOKEN } from '@env';
-import { EasyTeamProvider } from '@easyteam/ui';
+import React from "react";
+import { DarkTheme } from "@react-navigation/native";
+import { AUTH_TOKEN } from "@env";
+import { EasyTeamProvider } from "@easyteam/ui";
 
 //#region Navigation related setup
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { HomeScreen } from './screens/HomeScreen';
-import { ClockScreen } from './screens/ClockScreen';
-import { TimesheetScreen } from './screens/TImesheetScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator, NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { HomeScreen } from "./screens/HomeScreen";
+import { ClockScreen } from "./screens/ClockScreen";
+import { TimesheetScreen } from "./screens/TImesheetScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,7 +18,7 @@ export type RootStackParamList = {
   Timesheet: undefined;
 };
 
-export type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
+export type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, "Home">;
 //#endregion
 
 const employeesData = []; // fill this with your employees data
@@ -28,7 +28,7 @@ function App(): React.JSX.Element {
     <EasyTeamProvider token={AUTH_TOKEN} employees={employeesData}>
       <NavigationContainer theme={DarkTheme}>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'EasyTeam UI' }} />
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: "EasyTeam UI" }} />
           <Stack.Screen name="Clock" component={ClockScreen} />
           <Stack.Screen name="Timesheet" component={TimesheetScreen} />
         </Stack.Navigator>
